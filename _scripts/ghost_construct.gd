cfe3c2c661c12f38b4482dca9e8513e144b28ffc
Brendan Connolly <bsc23me@yahoo.com>
2026-04-11 16:40:13 -0600

    added grid manager and placement system

--- _scripts/ghost_construct.gd
+++ _scripts/ghost_construct.gd
@@ -1,23 +1,31 @@
-extends Node2D
+extends Sprite2D
 
-var grid_scale = 128
+@export var grid_scale = 128
 @export var scene_root: Node2D
 @export var tile_node: String
+@export var grid_manager_obj: Node2D
+
 var tile_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	tile_scene = load(tile_node)
+	self_modulate = Color(1.0, 1.0, 1.0, 0.373)
 	pass # Replace with function body.
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		position = snap(event.position, grid_scale)
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
+		if event.button_index == MOUSE_BUTTON_LEFT and !GridManager.has_tile(position):
 			var tile = tile_scene.instantiate()
 			tile.position = position
 			scene_root.add_child(tile)
+			GridManager.add_tile(tile.position, tile)
+		elif event.button_index == MOUSE_BUTTON_RIGHT:
+			var pos = Vector2i(position)
+			if GridManager.has_tile(pos):
+				GridManager.remove_tile(pos)
 			
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
