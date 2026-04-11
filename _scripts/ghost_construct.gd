ad794f27fc706b497b8019d875b89bc84b75c30b
Brendan Connolly <bsc23me@yahoo.com>
2026-04-11 17:27:27 -0600

    larger grid and rotations

--- _scripts/ghost_construct.gd
+++ _scripts/ghost_construct.gd
@@ -3,14 +3,13 @@ extends Sprite2D
 @export var grid_scale = 128
 @export var scene_root: Node2D
 @export var tile_node: String
-@export var grid_manager_obj: Node2D
 
 var tile_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	tile_scene = load(tile_node)
-	self_modulate = Color(1.0, 1.0, 1.0, 0.373)
+	self_modulate = Color(1.0, 1.0, 1.0, 0.376)
 	pass # Replace with function body.
 
 func _input(event):
@@ -20,13 +19,19 @@ func _input(event):
 		if event.button_index == MOUSE_BUTTON_LEFT and !GridManager.has_tile(position):
 			var tile = tile_scene.instantiate()
 			tile.position = position
+			tile.rotation = rotation
 			scene_root.add_child(tile)
 			GridManager.add_tile(tile.position, tile)
 		elif event.button_index == MOUSE_BUTTON_RIGHT:
 			var pos = Vector2i(position)
 			if GridManager.has_tile(pos):
 				GridManager.remove_tile(pos)
-			
+	if event is InputEventKey:
+		if event.keycode == KEY_R and event.pressed:
+			if event.shift_pressed:
+				rotation -= PI / 2
+			else:
+				rotation += PI / 2
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
